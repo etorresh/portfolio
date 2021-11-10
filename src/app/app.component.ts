7ea809b5c9e8b38b21ded8fb2089a5019e6476f5
etorresh <emiliotorreshinojosa@gmail.com>
2021-11-10 01:06:44 -0700

    added face animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('nextFace', [
+      state('show', style({
+        bottom: 0
+      })),
+      state('hide', style({
+        bottom: '250px'
+      })),
+      transition('show => hide', [
+        animate('0.5s ease-in')
+      ]),
+      transition('hide => show', [
+        animate('0.5s ease-out')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'portfolio-simplified';
+  title = 'Emilio Torres';
+  currentFace = 0;
+  restartFace = false;
+  myFaces = [true, false, false, false];
+  onFaceClick() {
+    this.myFaces[this.currentFace] = false;
+    if(this.currentFace == this.myFaces.length - 1) {
+      this.restartFace = true;
+      this.currentFace = 0;
+    } else {
+      this.restartFace = false;
+      this.currentFace++;
+    }
+    this.myFaces[this.currentFace] = true;
+  }
 }
