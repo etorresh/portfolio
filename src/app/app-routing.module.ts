6f4eaa93199ea4035810ff768a2752ddce37e5f1
etorresh <me@etorresh.com>
2022-07-21 23:19:17 -0600

    added blog support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
 
 @NgModule({
   declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ParticlesDirective} from "./directives/particles.directive";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ParticlesDirective } from './directives/particles.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParticlesDirective,
-    HomeComponent
-  ],
+  declarations: [AppComponent, ParticlesDirective, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
